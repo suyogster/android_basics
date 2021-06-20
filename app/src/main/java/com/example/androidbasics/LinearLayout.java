70e10da8e7289f525f5c4dfbd10ac35b2cc33c7b
Suyog Shrestha <sshrestha@deerhold.org>
2021-06-20 10:19:38 +0545

    Second Class Activity

--- app/src/main/java/com/example/androidbasics/LinearLayout.java
+++ app/src/main/java/com/example/androidbasics/LinearLayout.java
@@ -3,16 +3,12 @@ package com.example.androidbasics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class LinearLayout extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_relative_layout);
-
+        setContentView(R.layout.activity_linear_layout);
     }
-
 }
--- app/src/main/java/com/example/androidbasics/RelativeLayout.java
+++ app/src/main/java/com/example/androidbasics/RelativeLayout.java
@@ -4,15 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class RelativeLayout extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_relative_layout);
 
+        TextView username = findViewById(R.id.firstTextView);
+        username.setText("ModifiedComponent");
     }
-
 }
